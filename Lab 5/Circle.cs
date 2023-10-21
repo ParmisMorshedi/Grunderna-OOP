2fe882a10ffc4e91c3ae3dee59e9306ebab1c27e
Parmis Morshedi <parmis76.morshedi@gmail.com>
2023-10-21 23:29:21 +0200

    Add project files.

---
+++ Lab 5/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    internal class Circle
+    {
+        //Creat a field for radius of the circle
+        private double _radius;
+
+        //Creat a constructor
+        public Circle(double radie) 
+        {
+            _radius = radie;
+
+        }
+        // metode GetArea
+        public double GetArea()
+        {
+            double pi = Math.PI;
+            return pi * _radius * _radius;
+
+
+        }
+        //Extra utmaning
+
+        //Creat a metod to calculate perimeter
+        public double GetOmkrets() 
+        {
+            double pi = Math.PI;
+            return 2 * pi * _radius;
+
+        }
+        //Creat a metod to calculate volume
+        public double GetVolym() 
+        { 
+            double pi = Math.PI;
+            return (4.0/3.0 )* pi* _radius* _radius* _radius;
+           
+
+        }
+    }
+}
---
+++ Lab 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle mycircle1 = new Circle(5);
+            Circle mycircle2 = new Circle(6);
+
+         
+         
+
+            Console.WriteLine($"Arean på cirke1 med radie 5 är: {mycircle1.GetArea()}");
+            Console.WriteLine($"Arean på cirke2 med radie 6 är: {mycircle2.GetArea()}"); 
+
+            Console.WriteLine($"Omkretsen på cirke1 med radie 5 är: {mycircle1.GetOmkrets()}");
+            Console.WriteLine($"Omkretsen på cirke2 med radie 6 är: {mycircle2.GetOmkrets()}");
+
+            Console.WriteLine($"Volymen på cirke1 med radie 5 är: {mycircle1.GetVolym()}");
+            Console.WriteLine($"Volymen på cirke2 med radie 6 är: {mycircle2.GetVolym()}");
+
+        }
+    }
+}
